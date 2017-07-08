13e9c9c08ff67dde2cd4d0153e1839a49f751bd1
Timothy Dijamco <contact@timothydijamco.com>
2017-07-08 18:47:44 -0400

    Price history script now can make predictions on any day of the week, not just Fridays. Allowed for the user to input the date. Added comments.

--- pricehistory.js
+++ pricehistory.js
@@ -1,21 +1,32 @@
 var request = require('request');
 var brain = require("brain.js");
+var readlineSync = require('readline-sync');
 
+// Ask user for a date
+console.log("\nA GDAX day is from 8:00 PM to 8:00 PM on the next day (EDT).");
+console.log("Enter a date to get a prediction of the change in the price of");
+console.log("ETH from 8:00 PM on the provided day to 8:00 on the next day.\n");
+var date = readlineSync.question('Date (YYYY-MM-DD): ');
+console.log(""); // Newline
+
+// Get ETH price history, then create, train, and use neural network
 var priceHistory = [];
-
-addToPriceHistory(
-   '2016-07-01T11:00:00+00:00',
-   '2017-01-05T11:00:00+00:00',
+addToPriceHistory(         // The GDAX API limits the # of results to 200, so we need to use two API requests
+   '2016-07-01T12:00:00',
+   '2017-01-05T12:00:00',
    addToPriceHistory.bind(this,
-      '2017-01-06T11:00:00+00:00',
-      '2017-07-06T11:00:00+00:00',
+      '2017-01-06T12:00:00',
+      date + 'T12:00:00', // Get the price history up to the user-provided date
       function() {
+         // Generate training data from the price history
          var trainingData = generateTrainingData();
 
+         // Create a neural network
          var net = new brain.NeuralNetwork({
             hiddenLayers: [4]
          });
 
+         // Train the neural network on the training data
          net.train(trainingData, {
             errorThresh: 0.001,
             iterations: 250000,
@@ -24,56 +35,26 @@ addToPriceHistory(
             learningRate: 0.25
          });
 
-         testNeuralNetwork(net, [
-            0.5802273430250542,
-            0.7174021976448692,
-            0.5092905653720636,
-            0.6575053162049578,
-            0.47644453392389774,
-            0.3806701030927835
-        ], 0.5306944808123126);
+         // Generate the prediction. First get the inputs (for the neural
+         // network) from the price history of the days before the date that
+         // the user provided, then run the neural network on the inputs.
+         var inputs = [];
+         for (var i = 0; i < 7; i++) {
+            var lastIndex = priceHistory.length-1;
+            var currentPrice = priceHistory[lastIndex - i][4];
+            var previousPrice = priceHistory[lastIndex - i -1][4];
+            var normalized = generateNormalized(currentPrice, previousPrice);
+            inputs.push(normalized);
+         }
+         makePrediction(net, inputs);
 
-        testNeuralNetwork(net, [
-           0.5217486407099556,
-           0.4312496177136216,
-           0.41458128078817735,
-           0.40836295737728456,
-           0.6355892003004309,
-           0.5976433320569499
-        ], 0.45589691073562033);
-
-        testNeuralNetwork(net, [
-           0.494344957587182,
-           0.5028436018957345,
-           0.4924385633270322,
-           0.5085714285714285,
-           0.5103871576959396,
-           0.502803738317757
-        ], 0.5157116451016637);
-
-        testNeuralNetwork(net, [
-           0.494344957587182,
-           0.5028436018957345,
-           0.4924385633270322,
-           0.5085714285714285,
-           0.5103871576959396,
-           0.502803738317757
-        ], 0.4069959775948273);
-
-        testNeuralNetwork(net, [
-           0.5104166666666667,
-           0.4890034364261169,
-           0.47289784572619875,
-           0.5042857142857142,
-           0.3648648648648648,
-           0.544407894736842
-        ], 0.4839743589743589);
       }
    )
 );
 
+// Get price history data from the GDAX API and add it to priceHistory.
 function addToPriceHistory(start, end, callback) {
-   console.log("addToPriceHistory(" + start + ", " + end + ", ...)");
+   console.log("Waiting for the GDAX API...");
    request.get({
       url: 'https://api.gdax.com/products/ETH-USD/candles',
       qs: {
@@ -84,40 +65,48 @@ function addToPriceHistory(start, end, callback) {
          'User-Agent': 'Crypto Predictor Node.js"'
       }
    }, function(error, response, body) {
-      console.log("  response received.");
+      console.log("  Response received.");
       var json = JSON.parse(body);
-      json = json.reverse();
-      console.log("  adding " + json.length + " items to priceHistory...");
+      json = json.reverse(); // Reverse the array so that older data is first
       priceHistory = priceHistory.concat(json);
-      console.log("  priceHistory length is now: " + priceHistory.length);
-      console.log("  calling callback function...");
       callback();
    });
 }
 
+// Uses price history data in priceHistory to calculate the changes in the
+// ETH price for the appropriate days. Creates an array of objects containing
+// the price change information. This array can be passed to the neural network
+// to train it.
 function generateTrainingData() {
-   console.log("generateTrainingData()");
-   console.log("  priceHistory length: " + priceHistory.length);
+   console.log("Processing price history and generating training data...");
+
    var trainingData = [];
-   for (var i = 1; i < priceHistory.length-7; i+=7) {
-      //console.log("Week " + (i-1)/7 + ":");
+
+   // Scan backwards over weeks, starting from a week before the week that the
+   // date of interest is in.
+   for (var i = priceHistory.length-1-13; i >= 8; i-=7) {
+      console.log("Week " + Math.round((i-1)/7) + ":");
 
       var thisWeekInput = [];
       var thisWeekOutput = [];
-      for (var j = 0; j < 6; j++) {
-         var currentPrice = priceHistory[i+j][3];
-         var previousPrice = priceHistory[i+j-1][3];
-         var normalized = generateNormalizedInput(currentPrice, previousPrice);
-         //console.log(currentPrice + "    " + normalized);
+
+      // Generate inputs
+      for (var j = 0; j < 7; j++) {
+         var currentPrice = priceHistory[i+j][4];
+         var previousPrice = priceHistory[i+j-1][4];
+         var normalized = generateNormalized(currentPrice, previousPrice);
+         console.log("  " + currentPrice + "    " + ((normalized-0.5)*100) + "%");
          thisWeekInput.push(normalized);
       }
 
-      var fridayPrice = priceHistory[i+7][3];
-      var thursdayPrice = priceHistory[i+6][3];
-      var normalized = generateNormalizedInput(fridayPrice, thursdayPrice);
-      //console.log(fridayPrice + "    " + normalized);
+      // Generate output
+      var lastDayPrice = priceHistory[i+7][4];
+      var secondToLastDayPrice = priceHistory[i+6][4];
+      var normalized = generateNormalized(lastDayPrice, secondToLastDayPrice);
+      console.log("  " + lastDayPrice + "    " + ((normalized-0.5)*100) + "%");
       thisWeekOutput.push(normalized);
 
+      // Combine into a training data object
       var thisWeekData = {
          input: thisWeekInput,
          output: thisWeekOutput
@@ -128,12 +117,17 @@ function generateTrainingData() {
    return trainingData;
 }
 
-function generateNormalizedInput(currentPrice, previousPrice) {
+// Normalizes percentage like 1.05 (meaning 5% increase) or 0.75 (meaning 25%
+// decrease) into a number from 0 to 1.
+// 1.00 (meaning no change) becomes 0.5, 1.05 becomes 0.505, and so on.
+function generateNormalized(currentPrice, previousPrice) {
    var percentage = currentPrice/previousPrice;
    var normalized = percentage - 0.5;
    return normalized;
 }
 
+// Tests the neural network with the given inputs and writes to the console a
+// comparison of the predicted output and the actual output.
 function testNeuralNetwork(net, inputs, expectedOutput) {
    var output = net.run(inputs);
    var logString = "Actual: " + output;
@@ -143,4 +137,12 @@ function testNeuralNetwork(net, inputs, expectedOutput) {
       logString += "    !! Direction Error !!";
    }
    console.log(logString);
+}
+
+// Uses the neural network and the given inputs to make a prediction, which is
+// displayed in the console as a percentage.
+function makePrediction(net, inputs) {
+   var output = net.run(inputs);
+   var prediction = output-0.5;
+   console.log("\nPredicted change: " + (prediction*100).toFixed(3) + "%");
 }
