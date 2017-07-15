b16e62ce40fac94dfa1ae768f67b7ef0983d12ee
Timothy Dijamco <contact@timothydijamco.com>
2017-07-15 19:12:40 -0400

    Simple trading bot simulator now functional (iterates through days and asks a program to make a buy/sell decision each day based on the available ETH price history at the time, and keeps track of ETH and USD holdings.)

---
+++ ethbot.js
@@ -0,0 +1,163 @@
+var brain = require("brain.js");
+
+exports.makeDecision = function(priceHistoryDocs, currentDateTime) {
+   console.log("  Thinking...");
+   var network = trainNeuralNetwork(priceHistoryDocs, currentDateTime);
+
+   // If there was some error in creating the neural network, return 0 (decision: do nothing)
+   if (network == null) {
+      return 0;
+   }
+
+   // Get the day 6 days before currentDateTime. This will be the starting point for getting the
+   // 6 inputs (which are the changes in price in the 6 days leading up to the currentDateTime)
+   // for making the prediction.
+   var weekStartDateTime = addDays(currentDateTime, -6);
+   var weekStartIndex = findIndexOfDateTime(priceHistoryDocs, weekStartDateTime);
+
+   // Set up the inputs (in this case, the price changes in the 6 days leading up to the
+   // currentDateTime).
+   // The reliance on indices of priceHistoryDocs is buggy because a occaisionally, price
+   // history data points may be missing, causing the following input generation code to
+   // go out of bounds. Temporarily bypass this error with a try-catch statement.
+   try {
+      var inputs = [];
+      for (var i = 0; i < 7; i++) {
+         //console.log("  Day " + (i+1) + ":" + (new Date(priceHistoryDocs[weekStartIndex + (i*24)].time * 1000)));
+         var currentPrice = priceHistoryDocs[weekStartIndex + (i*24)].open;
+         var previousPrice = priceHistoryDocs[weekStartIndex + ((i-1)*24)].open;
+         var normalized = generateNormalized(currentPrice, previousPrice);
+         inputs.push(normalized);
+      }
+   } catch (err) {
+      console.log("  Error: " + err);
+      return 0;  // Do nothing
+   }
+
+   // Make the prediction
+   var output = network.run(inputs);
+
+   // De-normalize the output. For example, -0.12 means a decrease of 12% in the ETH price
+   // is predicted. 0.036 means an increase of 3.6% in the ETH price is predicted.
+   var prediction = output-0.5;
+
+   // This bot attempts to sell all ETH if a price drop is expected, and attempts to buy as
+   // much ETH as possible if a price increase is expected.
+   if (prediction < 0) {
+      return -1; // Sell max amount of ETH
+   } else if (prediction > 0) {
+      return 1; // Buy max amount of ETH
+   } else {
+      return 0; // Do nothing
+   }
+
+}
+
+function trainNeuralNetwork(priceHistoryDocs, anchorDateTime) {
+   var trainingData = generateTrainingData(priceHistoryDocs, anchorDateTime);
+
+   // If there was an error in generating the training data, then don't try to create
+   // a neural network.
+   if (trainingData == null) {
+      return null;
+   }
+
+   // Create a neural network
+   var net = new brain.NeuralNetwork({
+      hiddenLayers: [4]
+   });
+
+   // Train the neural network on the training data
+   net.train(trainingData, {
+      errorThresh: 0.001,
+      iterations: 250000,
+      log: false,
+      logPeriod: 10000,
+      learningRate: 0.25
+   });
+
+   return net;
+}
+
+// Generates training data using the given price history
+function generateTrainingData(priceHistoryDocs, anchorDateTime) {
+   //var anchorDateTime = new Date("2017-06-29T20:00:00-0400"); // June 30, 2017 at 8:00 PM EDT
+   var anchorDateTimeEpoch = anchorDateTime.getTime()/1000;
+   //console.log("anchorDateTimeEpoch: " + anchorDateTimeEpoch);
+
+   var anchorIndex = -1;
+   for (var i = priceHistoryDocs.length-1; i >= 0; i--) {
+      if (priceHistoryDocs[i].time == anchorDateTimeEpoch) {
+         anchorIndex = i;
+         break;
+      }
+   }
+
+   if (anchorIndex == -1) {
+      console.log("  Error: Could not find anchor datetime");
+      return null;
+   }
+
+   var trainingData = [];
+
+   // This for loop starts at anchorIndex-(14*24) to make sure that the second for loop, which
+   // loops forward, will not go out of bounds. Subtracting by (14*24) ensures that the second
+   // for loop starts at the correct day of the week.
+   for (var i = anchorIndex-(14*24); i >= (8*24); i-=(7*24)) {
+      var thisWeekInput = [];
+      var thisWeekOutput = [];
+
+      for (var j = 0; j < 7; j++) {
+         //console.log("     Day " + (j+1) + ":" + (new Date(priceHistoryDocs[i+(j*24)].time * 1000)));
+         var currentPrice = priceHistoryDocs[i+(j*24)].open;
+         var previousPrice = priceHistoryDocs[i+((j-1)*24)].open;
+         var normalized = generateNormalized(currentPrice, previousPrice);
+         thisWeekInput.push(normalized);
+      }
+
+      //console.log("2nd to last day: " + (new Date(priceHistoryDocs[i+(6*24)].time * 1000)));
+      //console.log("Last day: " + (new Date(priceHistoryDocs[i+(7*24)].time * 1000)));
+
+      var lastDayPrice = priceHistoryDocs[i+(7*24)].open;
+      var secondToLastDayPrice = priceHistoryDocs[i+(6*24)].open;
+      var normalized = generateNormalized(lastDayPrice, secondToLastDayPrice);
+      thisWeekOutput.push(normalized);
+
+      // Combine into a training data object
+      var thisWeekData = {
+         input: thisWeekInput,
+         output: thisWeekOutput
+      };
+      trainingData.push(thisWeekData);
+   }
+
+   return trainingData;
+}
+
+// Normalizes percentage like 1.05 (meaning 5% increase) or 0.75 (meaning 25%
+// decrease) into a number from 0 to 1.
+// 1.00 (meaning no change) becomes 0.5, 1.05 becomes 0.505, and so on.
+function generateNormalized(currentPrice, previousPrice) {
+   var percentage = currentPrice/previousPrice;
+   var normalized = percentage - 0.5;
+   return normalized;
+}
+
+// Finds the index of the price history data point in priceHistoryDocs for the
+// specified dateTime.
+function findIndexOfDateTime(priceHistoryDocs, dateTime) {
+   var epoch = dateTime.getTime()/1000;
+   for (var i = priceHistoryDocs.length-1; i >= 0; i--) {
+      if (priceHistoryDocs[i].time == epoch) {
+         return i;
+      }
+   }
+   return null;
+}
+
+
+function addDays(date, days) {
+  var newDate = new Date(date);
+  newDate.setDate(newDate.getDate() + days);
+  return newDate;
+}
--- testTradingBot.js
+++ testTradingBot.js
@@ -1,16 +1,121 @@
 var MongoClient = require('mongodb').MongoClient;
 var request = require('request');
 var brain = require("brain.js");
-var trainer = require("./trainPriceHistoryNN.js");
+var bot = require("./ethbot.js");
 
 var url = 'mongodb://localhost:27017/cryptopredictor';
 
+// Simulation parameters
+var startDateTime = new Date("2017-01-01T20:00:00-0400"); // January 1, 2017 at 8:00 PM EDT (7:00 PM EST)
+var endDateTime = new Date("2017-06-15T20:00:00-0400"); // June 15, 2017 at 8:00 PM EDT
+//--
+
+var priceHistoryDocs = [];
+
+// Load price history data, then start simulation
 MongoClient.connect(url, function(err, db) {
    var collection = db.collection('priceHistory');
-   collection.find({}).toArray(function(err, priceHistoryDocs) {
-
-      trainer.trainNNFromPriceHistory(priceHistoryDocs);
-
+   collection.find({}).toArray(function(err, documents) {
+      console.log("Error? " + err);
+      console.log("Price history (" + documents.length + " items) received.");
+      priceHistoryDocs = documents;
       db.close();
+
+      // BEGIN SIMULATION
+      simulate(startDateTime, endDateTime, 1000);
    });
 });
+
+
+function simulate(startDateTime, endDateTime, startingUSD) {
+   // Set up initial variables
+   console.log("\n====================");
+   console.log("BEGINNING SIMULATION.");
+   console.log("Starting at " + startDateTime.toISOString());
+   console.log("Ending at " + endDateTime.toISOString());
+   console.log("Starting with $" + startingUSD.toFixed(2));
+
+   var ethAmount = startingUSD / getPriceAtDateTime(startDateTime);
+   var usdAmount = 0;
+   console.log("             = " + ethAmount + " ETH");
+   console.log("Price at start: $" + getPriceAtDateTime(startDateTime).toFixed(2) + "/ETH");
+
+   // Loop through days until endDateTime is reached
+   var day = 1;
+   var currentDateTime = startDateTime;
+   while (currentDateTime < endDateTime) {
+      console.log("\n\nDay " + day + " (" + currentDateTime.toISOString() + "):");
+
+      // The trading bot shouldn't have access to price history data after currentDateTime, so
+      // generate a set of price history data from before currentDateTime.
+      var priceHistoryDocsAvailableToBot = getPHDBeforeOrAtDateTime(priceHistoryDocs, currentDateTime);
+
+      // Give information to bot and get a decision from it
+      var decision = bot.makeDecision(priceHistoryDocsAvailableToBot, currentDateTime);
+
+      // Decision can be from -1 to 1
+      //  - If negative, sell an amount of ETH proportional to the decision. For example,
+      //   if decision = -0.75, then sell 75% of the ETH.
+      //  - If positive, use an amount of USD proportional to the decision to buy ETH. For
+      //   Example, if decision = 0.45, then use 45% of USD to buy ETH.
+      if (Math.abs(decision) > 1) {
+         console.log("  Error: Invalid decision made!");
+         decision = 0; // Do nothing this day
+      }
+
+      var percentageDisplay = (Math.abs(decision)*100).toFixed(2) + "%";
+      var currentPrice = getPriceAtDateTime(currentDateTime);
+
+      // If currentPrice is not null, then it's safe to buy/sell based on the decision made.
+      if (currentPrice != null) {
+         if (decision < 0) {
+            var ethAmountToSell = ethAmount * (-decision);
+            var usdAmountToReceive = ethAmountToSell * currentPrice;
+            usdAmount += usdAmountToReceive;
+            ethAmount -= ethAmountToSell;
+            console.log("  Sold " + ethAmountToSell + " ETH (" + percentageDisplay + ") at $" + currentPrice + "/ETH, receiving $" + usdAmountToReceive);
+         } else {
+            var usdAmountToUse = usdAmount * decision;
+            var ethAmountToBuy = usdAmountToUse / currentPrice;
+            usdAmount -= usdAmountToUse;
+            ethAmount += ethAmountToBuy;
+            console.log("  Used $" + usdAmountToUse + " (" + percentageDisplay + ") at $" + currentPrice + "/ETH, receiving " + ethAmountToBuy + " ETH");
+         }
+      }
+
+      currentDateTime = addDays(currentDateTime, 1);
+      day++;
+   }
+
+}
+
+// Gets all price history data points in priceHistoryDocs (abbreviated to PHD here) that
+// were before dateTime.
+function getPHDBeforeOrAtDateTime(priceHistoryDocs, dateTime) {
+   var limitedPriceHistoryDocs = [];
+   for (var i = 0; i < priceHistoryDocs.length; i++) {
+      var date = new Date(priceHistoryDocs[i].time*1000);
+      if (date <= dateTime) {
+         limitedPriceHistoryDocs.push(priceHistoryDocs[i]);
+      }
+   }
+   return limitedPriceHistoryDocs;
+}
+
+// Get the price of ETH at the specified dateTime
+function getPriceAtDateTime(dateTime) {
+   var epoch = dateTime.getTime() / 1000;
+   for (var i = 0; i < priceHistoryDocs.length; i++) {
+      if (priceHistoryDocs[i].time == epoch) {
+         return priceHistoryDocs[i].open;
+      }
+   }
+   return null;
+}
+
+
+function addDays(date, days) {
+  var newDate = new Date(date);
+  newDate.setDate(newDate.getDate() + days);
+  return newDate;
+}
